7c98f3688b1fa703a273830e6d21f987962b6de9
Yurii Rysyn <zerocool_cs@ukr.net>
2020-09-12 14:52:51 +0300

    Updates

---
+++ precache-manifest.abf328cc7c2c739298a818dc21a2d7a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "097d3ec4ba92ef5ba3fba21c8b20e757",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ac3b1dee71df622a7293",
+    "url": "./static/css/main.132a992f.chunk.css"
+  },
+  {
+    "revision": "4ac321205352b05441f3",
+    "url": "./static/js/2.14aa6baa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.14aa6baa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac3b1dee71df622a7293",
+    "url": "./static/js/main.314f8902.chunk.js"
+  },
+  {
+    "revision": "0c165204287428b65967",
+    "url": "./static/js/runtime-main.a37292e9.js"
+  }
+]);
